4bf6ecc5f19ebe0554bed9d9e4aa9b830afece04
Serviox19 <servio19@gmail.com>
2016-02-15 21:43:44 -0500

    minor changes

--- db/seeds.sql
+++ db/seeds.sql
@@ -4,5 +4,7 @@
 SHOW COLUMNS from burgers;
 
 INSERT INTO burgers (burger_name, devoured) VALUES ('Big Mac', 'TRUE');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Turkey Burger', 'TRUE');
+
 
 
